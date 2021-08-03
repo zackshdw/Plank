d1b60e67c0b7c6841f3a9174c34cf046ca6c6ca5
Naughty's Zack <63024030+zackshdw@users.noreply.github.com>
2021-08-03 21:34:55 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.local/share/plank/themes &&
+cp -r theme-dark theme-light ~/.local/share/plank/themes &&
+echo
+echo Theme Successful Installed !!!
+echo
+echo You Can Select Installed Theme
+echo By Type $ plank --preferences
+echo
